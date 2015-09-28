b4f6ef65eede74ffdad9517cef3cc70f8ba9a574
werick <jackericmugoma@gmail.com>
2015-09-28 16:07:18 +0300

    updating error summary directive

--- app/scripts/formentry/directives/error-summary-directive.js
+++ app/scripts/formentry/directives/error-summary-directive.js
@@ -78,11 +78,12 @@ jshint -W098, -W003, -W068, -W004, -W033, -W030, -W117, -W069
           return Object.keys(field.formControl.$error).map(function(error) {
             // note, this only works because the all the field types have been explicityly defined.
             // console.log('Erroorr', error);
-            // console.log('selected field label ', field.templateOptions.label)
+            console.log('selected field label ', field.templateOptions.label)
             var msg
             if(error === 'max')  msg = 'The maximum value allowed is ' + field.templateOptions.max;
             else if(error === 'min') msg = 'The minimum value allowed is ' + field.templateOptions.min;
             else msg = field.validation.messages[error]();
+
             return msg;
           }).join(', ');
         }
